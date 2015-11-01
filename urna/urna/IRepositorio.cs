14f295d93c4856455e9f32c071b6e7a031660af0
Marcos Koch <marcos_koch@hotmail.com>
2015-11-01 14:55:03 -0200

    feat:(TrabalhoUrna) - Adicionado e configurado com banco de dados projeto inicial.

---
+++ urna/urna/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace urna
+{
+    interface IRepositorio<T>
+    {
+        T BuscarPorId(int id);
+    }
+}
---
+++ urna/urna/Program.cs
@@ -0,0 +1,44 @@
+﻿using DbExtensions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace urna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "UPDATE Cargo SET Nome = @paramNome WHERE IDCargo = @paramIdCargo";
+
+                comando.AddParameter("paramNome", "Prefeito");
+                comando.AddParameter("paramIdCargo", 1);
+
+                connection.Open();
+
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                transacao.Complete();
+
+                connection.Close();
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}

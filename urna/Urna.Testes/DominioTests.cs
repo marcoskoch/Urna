0d97dfe66d04c07eab34999e6b04e1b5ad3f8b0c
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2015-11-03 01:13:25 -0200

    refact(Urna.Teste): alterar projeto de teste

---
+++ urna/Urna.Testes/DominioTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Urna.Testes
+{
+    [TestClass]
+    public class DominioTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

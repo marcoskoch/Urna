e4334802c731d9362202f767289b5857949a7bcb
Marcos Koch <marcos_koch@hotmail.com>
2015-11-02 22:05:57 -0200

    adicionado urna dominio

--- urna/Urna.Dominio/Properties/AssemblyInfo.cs
+++ urna/Urna.Dominio/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87cae09a-7e57-4fe5-8268-bacb04fd16f8")]
+[assembly: Guid("0a6f7fa0-f241-431a-9487-b7de896ebbe3")]
 
 // Version information for an assembly consists of the following four values:
 //
